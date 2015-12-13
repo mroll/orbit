e4423b644792b7c55f958b32449ad321d017fb76
mroll <mpr56@drexel.edu>
2015-12-13 18:06:02 -0500

    adding readme and picture

---
+++ readme.md
@@ -0,0 +1,20 @@
+# General
+
+The goal of this project is to support real time simulation of celestial mechanics, at first using Newtonian equations of motion. This project came out of an idea I had for building a text-rpg-like space battle game that is played in real time and makes the player base their strategy on things like
+
+* position of objects in the system (planets, asteroids),
+* amount of fuel, ammo,
+* ship damage,
+* gravitational effects of large objects (stars, planets, etc.).
+
+# Structure
+
+Rather than tackle the n-body problem, I think I will start by building good support for general two-body problem situations, mostly involving one mass that is much larger than the other. This simplifies the calculation of orbits of many planets and ships around a single comparatively massive star. Eventually I do plan on calculating orbital perturbation as small ships pass through the gravitational field of larger objects like planets or moons.
+
+My plan is to build a physics engine that will handle all the math for each two-body problem. The game client will pass parameters for a two-body problem that needs solving, and the engine will spit out the answer. I think that separation will simplify building clients.
+
+# Progress
+
+I am able to define orbits in terms of five elements: inclination, longitude of the ascending node, argument of periapsis, eccentricity, and semimajor axis. Then, given those initial conditions, I can find the point in 3d space of a body at an arbitrary point in time through some math. Here is a plot of three orbits I generated using this code.
+
+![orbit.png](orbits.png)
--- twobody.lisp
+++ twobody.lisp
@@ -4,6 +4,9 @@
 (defun get-y (pt)
   (cadr pt))
 
+(defun get-z (pt)
+  (caddr pt))
+
 (defun sqr (n)
   (* n n))
 
@@ -12,130 +15,22 @@
         (delta_y (- (get-y p2) (y p1))))
     (sqrt (+ (sqr delta_x) (sqr delta_y)))))
 
-(defun vlen (v)
-  (distance v '(0 0)))
+(defun distance3d (p1 p2)
+  (let ((delta-x (- (get-x p2) (get-x p1)))
+        (delta-y (- (get-y p2) (get-y p1)))
+        (delta-z (- (get-z p2) (get-z p1))))
+    (sqrt (+ (sqr delta-x) (sqr delta-y) (sqr delta-z)))))
 
-(defun er (theta)
-  (list (cos theta) (sin theta)))
+(defvar G (* 6.674 (expt 10 -11)))
 
-(defun et (theta)
-  (list (- 0 (sin theta)) (cos theta)))
+(defun body (m initial-pos)
+  (list m initial-pos))
 
-(defun dotproduct (x v)
-  (loop for y in v
-        collect (* x y)))
+(defun get-mass (body)
+  (car body))
 
-(defun r (radius theta)
-  (dotproduct radius (er theta)))
-
-;(loop for i from 1 to 20 by 0.1 do
-;      (let ((pt (r 1 i)))
-;        (format t "~A ~A~%" (get-x pt) (get-y pt))))
-
-; for a planet with position vector _r_,
-; the its equation of motion about a single
-; center of gravity, M, is given by:
-;
-;       d''r    -GM 
-;       ---- = ----- * _r_
-;       dt^2    r^3
-;
-;  where r is the length of _r_, G is the
-;  gravitational constant, and M is the
-;  solar mass.
-;
-;  Polar Coordinates
-;  -----------------
-;
-;  We can determine the instantaneous position
-;  of our planet in the x,y plane in terms of
-;  standard Cartesian coordinates, (x, y), or
-;  polar coordinates, (r, theta). Here,
-;  r = sqrt(x^2 + y^2) and theta = tan^-1(y/x).
-;  It is helpful to define two unit vectors,
-;  er = _r_/r and et = ez x er, at the instant-
-;  aneous position of the planet. The first
-;  always points radially away from the origin,
-;  whereas the second is normal to the first, in
-;  the direction of increasing theta. The
-;  Cartesian components of er and et are
-;
-;       er = (cos(theta), sin(theta))       (218)
-;       et = (-sin(theta), cos(theta))      (219)
-;
-;  respectively.
-;
-;  We can write the position vector of our
-;  planet as
-;
-;       _r_ = r * er.                       (220)
-;
-;  Thus, the planet's velocity becomes
-;
-;             d_r_
-;       _v_ = ---- = (r' * er) + (r * er'). (221)
-;              dt
-;
-;  Note that er has a non-zero time-derivative
-;  because its direction _changes_ as the planet
-;  moves around. As is easily demonstrated, from
-;  differentiating (218) with respect to time,
-;
-;       er' = theta'(-sin(theta), cos(theta))
-;           = theta' * et.
-;
-;  Thus,
-;
-;       _v_ = (r' * er) + (r * theta' * et).
-;
-;  Now, the planet's acceleration is written
-;
-;           d_v_   d^2_r_
-;       a = ---- = ------ 
-;            dt     dt^2
-;
-;         = (r''*er) + (r'*er') + (r'theta' + r*theta'')*et + (r*theta'et').
-;
-;  Again, et has a non-zero time-derivative
-;  because its direction _changes_ as the planet
-;  moves around.
-;
-;  Differentiation of Equation (219) with respect
-;  to time yields
-;
-;       et' = theta'*(-cos(theta), -sin(theta))
-;           = -theta'*er.
-;
-;  Hence,
-;
-;       _a_ = (r'' - r*theta'^2)*er + (r*theta'' + 2*r'*theta')*et.
-;
-;  It follows that the equation of motion of
-;  our planet, (212), can be written
-;
-;       _a_ = (r'' - r*theta'^2)*er + (r*theta'' + 2*r'*theta')*et
-;             -GM
-;           = --- * er.
-;             r^2
-;
-;  Since er and et are mutually orthogonal, we
-;  can separately equate the coefficients of both,
-;  in the above equation, to give a
-;  _radial equation of motion_,
-;
-;       r'' - r*r*theta'^2
-;         -GM
-;       = --- * er.
-;         r^2
-;
-;  And a _tangential equation of motion_,
-;
-;       r*theta'' + r*r'*theta' = 0.
-;
-;
-
-(defun ship (m a ecc _t)
-  (list m a ecc _t))
+(defun get-initial-pos (body)
+  (cadr body))
 
 (defun orbit (i ecc w bigw a mu bigt)
   (list i ecc w bigw a mu bigt))
@@ -147,6 +42,9 @@
 (defun get-a (orbit) (nth 4 orbit))
 (defun get-mu (orbit) (nth 5 orbit))
 (defun get-bigt (orbit) (nth 6 orbit))
+
+(defun solve-mu (m1 m2)
+  (* G (+ m1 m2)))
 
 (defun solve-n (mu a)
   (* (sqrt mu) (expt a (/ -3 2))))
@@ -160,20 +58,24 @@
 (defun solve-r (a ecc E)
   (* a (- 1 (* ecc (cos E)))))
 
+(defun solve-a (body center)
+  (distance3d (get-initial-pos body)
+              (get-initial-pos center)))
+
 (defun Kepler (M ecc)
   (do ((i 0 (1+ i))
        (ecc-rads (/ (* 180 ecc) pi) ecc-rads)
        (E M (+ M (* ecc-rads (sin E)))))
     ((> i 5) E)))
 
-(defun a (ship)
-  (cadr ship))
+(defun a (body)
+  (cadr body))
 
-(defun ecc (ship)
-  (caddr ship))
+(defun ecc (body)
+  (caddr body))
 
-(defun bigt (ship)
-  (cadddr ship))
+(defun bigt (body)
+  (cadddr body))
 
 (defun to-coord (decimal)
   (multiple-value-bind (deg tmp) (floor decimal)
@@ -222,24 +124,52 @@
         (i (get-i orbit)))
     (values-list (get-3d-coords (solve-r orbit f) w bigw f i))))
 
-(defun getpos (ship time mu)
-  (let* ((ecc (ecc ship))
-         (a (a ship))
-         (n (solve_n mu (a ship)))
-         (M (solve_M n time (bigt ship)))
+(defun getpos (body time mu)
+  (let* ((ecc (ecc body))
+         (a (a body))
+         (n (solve_n mu (a body)))
+         (M (solve_M n time (bigt body)))
          (E (Kepler M ecc))
          (r (solve_r a ecc E))
          (V (solve_V mu r a)))
     (values r V n M E)))
 
+(defmacro newbody (name mass initial-position)
+  `(defvar ,name (body ,mass ,initial-position)))
+
+; the semi-major axis, a, will be defined by the distance
+; from the star to the initial position of the body.
+(defmacro make-orbit (name body i ecc w bigw bigt)
+  (let ((muvar (gensym))
+        (avar  (gensym)))
+    `(let ((,muvar (solve-mu (get-mass ,body)
+                             (get-mass sol)))
+           (,avar  (solve-a ,body sol)))
+       (defvar ,name (orbit ,i ,ecc ,w ,bigw ,avar ,muvar ,bigt)))))
+
+(defun write-orbit (orbit fname steps step-size)
+  (with-open-file (fp fname
+                      :direction :output
+                      :if-exists :overwrite
+                      :if-does-not-exist :create)
+    (loop for i from 0 upto steps by step-size do
+          (multiple-value-bind (x y z) (spatial-orientation orbit i)
+            (format fp "~,3F ~,3F ~,3F~%" x y z)))))
+
+(newbody enterprise 10 '(20 20 20))
+(newbody serenity 4 '(20 10 15))
+(newbody deathstar 30 '(7 5 5))
+(newbody sol 10000 '(0 0 0))
+
+;(make-orbit enterprise-orbit enterprise 30 0.9 4 10 2.4 1)
+;(make-orbit serenity-orbit serenity 10 0.75 10 7 3 3)
+
+(make-orbit enterprise-orbit enterprise 30 0.9 4 2.4 1)
+(make-orbit serenity-orbit serenity 10 0.75 10 3 3)
+(make-orbit deathstar-orbit deathstar 0 0.2 90 0 0)
+
 ; orbit args: i ecc w bigw a mu bigt
-(defvar o1 (orbit 30 0.9 4 10 2.4 20 1))
-(defvar o2 (orbit 10 0.75 2.4 40 4 60 4))
 
-; (loop for x from 0 upto 100 by 0.1 do
-;       (multiple-value-bind (x y z) (spatial-orientation o1 x)
-;         (format t "~,3F ~,3F ~,3F~%" x y z)))
-
-(loop for x from 0 upto 100 by 0.1 do
-      (multiple-value-bind (x y z) (spatial-orientation o2 x)
-        (format t "~,3F ~,3F ~,3F~%" x y z)))
+(write-orbit enterprise-orbit "enterprise.orbit" 100 0.1)
+(write-orbit serenity-orbit "serenity.orbit" 100 0.1)
+(write-orbit deathstar-orbit "deathstar.orbit" 100 0.1)
